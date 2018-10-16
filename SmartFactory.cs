e061d9e74e0883ce7f23f3c088da548300b52f18
karl <lux_ceo@outlook.kr>
2018-10-16 23:00:00 +0900

    This is the first commit.

---
+++ SmartFactory.cs
@@ -0,0 +1,17 @@
+namespace karl 
+{
+	public class SmartFactory 
+	{
+		public static void Main(String[] args) 
+		{
+			
+			var number1 = 10;
+			var number2 = 20.0D;
+			var summation = number1 + number2;
+			
+			System.Console.WrtieLine($"{0}", summation);
+			System.Console.WriteLine($"{\n}");
+			
+		}
+	}
+}
